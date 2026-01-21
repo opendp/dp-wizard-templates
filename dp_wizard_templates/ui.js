7212d16544bfd32ec693796bc4e8baa6f3f70f87
Chuck McCallum <mccallucc@gmail.com>
2026-01-21 11:41:29 -0500

    use jquery to find tags

--- dp_wizard_templates/ui.js
+++ dp_wizard_templates/ui.js
@@ -1,1 +1,17 @@
-alert('hello world')
+// Wrap in anonymous function to avoid global pollution.
+// TODO: Convert to ESM.
+(() => {
+    const prefix = "celltag_"
+    var tags = new Set();
+    $(`div[class*='${prefix}']`).each((i, el) => {
+        const el_tags = new Set(
+            $(el)
+            .attr("class")
+            .split(" ")
+            .filter((class_name) => class_name.startsWith(prefix))
+            .map((class_name) => class_name.replace(prefix, ""))
+        );
+        tags = tags.union(el_tags)
+    });
+    console.log(tags);
+})();
