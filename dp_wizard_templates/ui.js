27c9a3524884f3d3d81fc03ef9548a560a7700d7
Chuck McCallum <mccallucc@gmail.com>
2026-01-29 12:08:26 -0500

    update JS

--- dp_wizard_templates/ui.js
+++ dp_wizard_templates/ui.js
@@ -1,32 +1,23 @@
 // Wrap in anonymous function to avoid global pollution.
 // TODO: Convert to ESM.
 (() => {
-    const prefix = "celltag_"
-    var tags = new Set();
-    $(`div[class*='${prefix}']`).each((i, el) => {
-        const el_tags = new Set(
-            $(el)
-            .attr("class")
-            .split(" ")
-            .filter((class_name) => class_name.startsWith(prefix))
-            .map((class_name) => class_name.replace(prefix, ""))
-        );
-        tags = tags.union(el_tags)
-    });
+    const $first_cell = $(".jp-Cell").first();
+    var frontmatter;
 
-    const tag_values = tags.values().toArray().sort();
-
-    if (tag_values.length === 0) {
-        // Do not inject HTML if there will be no options:
+    try {
+        frontmatter = JSON.parse($first_cell.text());
+    } catch (error) {
+        console.log("No JSON frontmatter in first cell; No further processing.", error);
         return;
     }
+    $first_cell.remove();
 
-    const options_html = (
-        tag_values
-        // Don't need to escape because tag is from a class name:
-        .map((tag) => `<option value="${tag}">${tag.replaceAll("_", " ")}</option>`)
-        .join("\n")
-    ) + '<option value="">(none)</option>';  // Empty value is falsy in show_only().
+    const tagmap = frontmatter?.tagmap;
+
+    const $select = $("<select>");
+    Object.entries(tagmap).forEach(([label, tags]) => {
+        $select.append($("<option>", {value: tags.join("|")}).text(label));
+    })
 
     // HTML skeleton is just copy-paste from notebook source:
     // Looks ok, but the semantics aren't correct.
@@ -39,26 +30,25 @@
                     Show:
                 </div>
                 <div class="jp-RenderedHTMLCommon jp-RenderedMarkdown jp-MarkdownOutput" data-mime-type="text/markdown">
-                    <select>
-                        ${options_html}
-                    <select>
+                    <select>${$select.html()}</select>
                 </div>
             </div>
         </div>
     `);
 
-    function show_only(tag) {
+    function show_only(tags) {
+        const prefix = "celltag_";
         $(`div[class*='${prefix}']`).hide();
-        if (tag) {
+        tags.forEach((tag) => {
             $(`div.${prefix}${tag}`).show();
-        }
+        });
     }
 
-    const default_tag = tag_values[0];
-    show_only(default_tag);
+    const default_tags = Object.values(tagmap)[0];
+    show_only(default_tags);
 
     $("select").on("change", (event) => {
-        const tag = event.target.value;
-        show_only(tag);
+        const tags = event.target.value.split("|");
+        show_only(tags);
     })
 })();
